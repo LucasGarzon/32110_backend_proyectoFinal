6acac52dd02d3e3fa612c843255188a799e880bf
Lucas Garzon <garzon.lucas.m@gmail.com>
2022-09-27 14:37:04 -0300

    feat: productos con todos los metodos impelementados | sin validaciones

--- src/controllers/productManager.js
+++ src/controllers/productManager.js
@@ -54,6 +54,52 @@ class ManagerProducto {
     }
   }
 
+  findById = async (id) => {
+    let productos = []
+    if (fs.existsSync(pathToFile)) {
+      let data = await fs.promises.readFile(pathToFile, 'utf-8')
+      productos = JSON.parse(data)
+      id = parseInt(id)
+      return productos.find(item => item.id === id)
+    } else {
+      return {error:"No existen productos"}
+    }
+  } 
+
+  update = async (id, product) => {
+    let productos = []
+    if (fs.existsSync(pathToFile)) {
+      let data = await fs.promises.readFile(pathToFile, 'utf-8')
+      productos = JSON.parse(data)
+      id = parseInt(id)
+      let newProducts = productos.map(item => {
+        if (item.id === id) {
+          return {
+            id, 
+            ...product
+          }
+        } else return item
+      })
+      productos = newProducts
+      await fs.promises.writeFile(pathToFile, JSON.stringify(productos, null, 2))
+      return this.findById(id)
+    } else {
+      return {error:"No existen productos"}
+    }
+  }
+
+  delete = async (id) => {
+    let productos = []
+    if (fs.existsSync(pathToFile)) {
+      let data = await fs.promises.readFile(pathToFile, 'utf-8')
+      productos = JSON.parse(data)
+      id = parseInt(id)
+      let newProducts = productos.filter(item => item.id !== id)
+      productos = newProducts
+      await fs.promises.writeFile(pathToFile, JSON.stringify(productos, null, 2))
+      return productos
+    }
+  }
 }
 
 module.exports = ManagerProducto
--- src/router/router_products.js
+++ src/router/router_products.js
@@ -8,8 +8,11 @@ router.get('/', (req, res) => {
   manager.findAll().then(result => res.send({status: 200, message: 'Hello GET By Id', products:result}))
 })
 
-router.get('/:id', (req, res) => {
-  res.send({status: 200, message: 'Hello GET By Id'})
+router.get('/:id', (req, res) => { 
+  manager.findById(req.params.id).then(result => {
+    if (!result) return res.send({error: 'product was not found'})
+    res.send({status: 200, message: 'Hello GET By Id', product:result})
+  })
 })
 
 router.post('/', (req, res) => {
@@ -18,11 +21,18 @@ router.post('/', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-  res.send({status: 200, message: 'Hello PUT'})
+  manager.update(req.params.id, req.body).then(result => {
+    // if (!req.body.nomb || !req.body.price || !req.body.thumbnail) return res.send({error: 'data is required'})
+    if (!result) return res.send({error: 'product was not found'})
+    res.send({status: 200, message: 'Hello PUT', productUpdated:result}) 
+  })
 })
 
 router.delete('/:id', (req, res) => {
-  res.send({status: 200, message: 'Hello DELETE'})
+  manager.delete(req.params.id).then(result => {
+    if (!result) return res.send({error: 'product was not found'})
+    res.send({status: 200, message: 'Product deleted', products:result})
+  })
 })
 
 module.exports = router
